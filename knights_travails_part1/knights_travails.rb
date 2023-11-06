4e3603240dc7175926fef7dc453a4ffbec8db696
Mujahed Ali <mujahed97@gmail.com>
2023-11-06 11:55:07 -0500

    build_move_tree

--- knights_travails_part1/knights_travails.rb
+++ knights_travails_part1/knights_travails.rb
@@ -1,8 +1,19 @@
 require_relative "/poly_node_tree/tree_node.rb"
 
+# Class Constants knight moves based on gird position
+
 class KnightPathFinder
-    def initialize(position)
-        
+    def initialize(start)
+        @start = start
+    
+    end
+    
+    def build_move_tree
+        self.root_node = PolyTreeNode.new(@start)
+    end
+
+    def find_path(position)
+
     end
 
 end
